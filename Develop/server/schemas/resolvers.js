e9be94cfff80b721227a8f53cf234ad44beeb113
RyStreet <ryanstreet122@gmail.com>
2023-02-21 15:22:44 -0600

    working on resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,9 +1,34 @@
-const {User, Book} = require('../models')
+const { AuthenticationError } = require('apollo-server-express');
+const {User, Book} = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-      me: async () => {
+      findAllUsers: async () => {
         return User.find({});
+      }
+    },
+
+    Mutation: {
+      addUser: async (parent, {username, email, password}) => {
+        const user = await User.create({username, email, password});
+        const token = signToken(user);
+        return {token, user};
+      },
+      login: async (parent, {email, password}) => {
+        const user = await User.findOne({email});
+
+        if(!user){
+          throw new AuthenticationError("No user found with this email")
+        }
+        const correctPW = await user.isCorrectPassword(password)
+
+        if(!correctPW){
+          throw new AuthenticationError('Incorrect username or password')
+        }
+        const token = signToken(user)
+
+        return {token, user}
       }
     }
 }
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -3,7 +3,7 @@ const {gql} = require('apollo-server-express');
 const typeDefs = gql`
 
 type Query{
-    me: [User]
+    findAllUsers: [User]
 }
 type User{
     _id: ID
