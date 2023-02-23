3b522a5da53afefbe10b82bc26d9d55f85130e08
RyStreet <ryanstreet122@gmail.com>
2023-02-23 14:41:51 -0600

    login and create user mutations

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,1 +1,38 @@
 import {gql} from "@apollo/client"
+
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password){
+        token
+        user{
+            _id
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!, $email, String!){
+    addUser(username: $username, password: $password, email: $email){
+        user{
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                _id
+                bookdId
+                description
+                title
+                image
+                link
+                author
+            }
+        }
+        token
+    }
+}
+`;
+
+
+
