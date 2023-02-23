2591fc7b61f4b6fd1687e1db26a6e1f490cef159
RyStreet <ryanstreet122@gmail.com>
2023-02-23 14:50:57 -0600

    working on get me user query

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -4,8 +4,13 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-      me: async () => {
-        return User.find({});
+      me: async (parent, args, context) => {
+        if(context.user) {
+          const userData = await User.findOne({_id: context.user._id})
+          .select('-__v -password')
+          return userData
+        }
+        throw new AuthenticationError('User not logged in')
       }
     },
 
