dc768bfa513cfa55093e0e613d9826563bae3ad1
RyStreet <ryanstreet122@gmail.com>
2023-02-23 18:08:53 -0600

    clint side query added

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -1,3 +1,20 @@
 import { gql } from "@apollo/client";
 
-export const GET_ME
+export const GET_ME = gql `
+
+query me{
+    _id
+    username
+    email
+    bookCount
+    savedBooks: {
+        _id
+        bookId
+        description
+        title
+        image
+        link
+        authors
+    }
+}
+`
